3a1b09e98e208cca21851df60765702c65d92636
collin <c.stasiuk94@hotmail.com>
2020-02-09 12:14:34 -0500

    hellooo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //hello stuff
+            Console.WriteLine("Changes")
         }
     }
 }
